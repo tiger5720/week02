3a673a772c1212768af8a67ec17d7901743a8716
tiger5720 <tiger5720@naver.com>
2023-03-11 11:18:45 +0900

    second

--- src/ocp/Pokemon.java
+++ src/ocp/Pokemon.java
@@ -1,5 +1,42 @@
 package ocp;
 
-public class Pokemon {
+public abstract class Pokemon {
+    //protected String name;
+    private String name;
+    protected int level;
+    protected int hp;
 
+    public Pokemon(String name, int level, int hp) {
+        this.name = name;
+        this.level = level;
+        this.hp = hp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    // 추상 메서드
+    public abstract void attack();
+    public abstract void evolve();
 }
