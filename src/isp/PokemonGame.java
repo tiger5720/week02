cef6be13b8d6633397fa0e2eff0baf7eb26fb3cb
tiger5720 <tiger5720@naver.com>
2023-03-18 09:55:42 +0900

    week03 3nd isp after

---
+++ src/isp/PokemonGame.java
@@ -0,0 +1,9 @@
+package isp;
+
+public class PokemonGame {
+    public static void main(String[] args) {
+        Pikkachu pikkachu = new Pikkachu();
+        pikkachu.defend();
+        pikkachu.attack();
+    }
+}
